16d62d34863cc8e11873d0e4ec155fe0eeffc084
jinsuSang <climbplant39@kakao.com>
2021-11-17 17:15:09 +0900

    feat: add nav in Movies

--- src/views/movies/Movies.vue
+++ src/views/movies/Movies.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <span>
-      <button @click="logout">Logout</button>
-    </span>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Menu" right>
+          <b-dropdown-item href="#">Home</b-dropdown-item>
+          <b-dropdown-item href="#">Search</b-dropdown-item>
+          <b-dropdown-item href="#">Recommend</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="logout">Logout</b-dropdown-item>
+          <b-dropdown-item href="#">Sign out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
@@ -12,7 +27,7 @@ export default {
   methods: {
     logout: function () {
       this.$store.dispatch('logout')
-      this.$router.push({name: 'Signup'})
+      this.$router.push({name: 'Login'})
     },
   },
 };

352a4c61d0cba3f342632443c73223f88fcae112
seongbiny <s30ngb1n@gmail.com>
2021-11-18 14:16:10 +0900

    feat: navbar

--- src/components/layout/MovieNav.vue
+++ src/components/layout/MovieNav.vue
@@ -1,30 +1,38 @@
 <template>
-    <b-navbar type="dark" variant="dark" class="nav fixed-top">
-      <b-navbar-nav>
-        <b-nav-item> 
-         <router-link to="/movies" class="text-decoration-none">Home</router-link>
-        </b-nav-item>
+  <b-navbar type="dark" variant="dark" class="nav">
+    <b-navbar-nav>
+      <b-nav-item>
+        <router-link to="/movies" class="text-decoration-none"
+          >Home</router-link
+        >
+      </b-nav-item>
 
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Menu" right>
-          <b-dropdown-item><router-link to="/movies" class="text-decoration-none"
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Menu" right>
+        <b-dropdown-item
+          ><router-link to="/movies" class="text-decoration-none"
             >Home</router-link
-          ></b-dropdown-item>
-          <b-dropdown-item><router-link to="/research" class="text-decoration-none"
+          ></b-dropdown-item
+        >
+        <b-dropdown-item
+          ><router-link to="/research" class="text-decoration-none"
             >Search</router-link
-          ></b-dropdown-item>
-          <b-dropdown-item><router-link to="/recommend" class="text-decoration-none"
+          ></b-dropdown-item
+        >
+        <b-dropdown-item
+          ><router-link to="/recommend" class="text-decoration-none"
             >Recommend</router-link
-          ></b-dropdown-item>
-        </b-nav-item-dropdown>
+          ></b-dropdown-item
+        >
+      </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="/profile">Profile</b-dropdown-item>
-          <b-dropdown-item @click="logout">Logout</b-dropdown-item>
-          <b-dropdown-item href="#">Sign out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-navbar>
+      <b-nav-item-dropdown text="User" right>
+        <b-dropdown-item href="/profile">Profile</b-dropdown-item>
+        <b-dropdown-item @click="logout">Logout</b-dropdown-item>
+        <b-dropdown-item href="#">Sign out</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
 </template>
 
 <script>

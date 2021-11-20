e5d6647ceb80f22a27b32f6bb5eca8b8013aba39
jinsuSang <climbplant39@kakao.com>
2021-11-20 18:57:12 +0900

    feat: update video frame

--- src/components/VideoItem.vue
+++ src/components/VideoItem.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <iframe 
+    id="player" 
+    type="text/html"
+    width="640"
+    height="360"
+    :src="videoUrl"
+    frameborder="0">
+  </iframe>
 </template>
 
 <script>
@@ -7,19 +14,31 @@ import axios from "axios";
 
 export default {
   name: "VideoItem",
-  data: function () {
+  data() {
     return {
-      video: null,
-    };
+      videoId : null,
+      baseUrl : "http://www.youtube.com/embed/"
+    }
   },
-  methods: {
-    created() {
+  props: {
+    title : String
+  },
+  computed: {
+    videoUrl(){
+      return `https://youtube.com/embed/${this.videoId}`;
+    }
+  },
+  created() {
       axios({
         method: "post",
         url: "http://127.0.0.1:8000/movies/youtube/",
         headers: this.$store.getters["setToken"],
+        data: { title: this.title }
+      }).then(res => {
+        console.log(res.data)
+      }).catch(err => {
+        console.error(err)
       });
-    },
   },
 };
 </script>

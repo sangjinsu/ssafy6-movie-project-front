4b6154999bb9f5c83e0ec08afcb7624566bc141b
jinsuSang <climbplant39@kakao.com>
2021-11-22 20:08:08 +0900

    feat: delete searchbar

--- src/components/layout/MovieNav.vue
+++ src/components/layout/MovieNav.vue
@@ -14,8 +14,6 @@
       <b-nav-item to="/recommend" class="text-decoration-none p-2 bd-highlight"
         >Recommend</b-nav-item
       >
-
-      <v-select v-model="movieSelected" :options="options" class="p-2" @keyup.enter="search"></v-select>
       
       <b-nav-item-dropdown
         id="my-nav-dropdown"
@@ -47,51 +45,6 @@ import axios from 'axios';
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 export default {
   name: "MovieNav",
-  data() {
-    return {
-      movieSelected: "검색",
-      movieNames: [],
-      // options: [
-      //   { label: "액션", id: 28 },
-      //   { label: "모험", id: 12 },
-      //   { label: "애니메이션", id: 16 },
-      //   { label: "범죄", id: 80 },
-      //   { label: "다큐멘터리", id: 99 },
-      //   { label: "드라마", id: 18 },
-      //   { label: "가족", id: 10751 },
-      //   { label: "판타지", id: 14 },
-      //   { label: "역사", id: 36 },
-      //   { label: "공포", id: 27 },
-      //   { label: "음악", id: 10402 },
-      //   { label: "미스터리", id: 9648 },
-      //   { label: "로맨스", id: 10749 },
-      //   { label: "SF", id: 878 },
-      //   { label: "TV 영화", id: 10770 },
-      //   { label: "스릴러", id: 53 },
-      //   { label: "전쟁", id: 10752 },
-      //   { label: "서부", id: 37 },
-      // ],
-    }
-  },
-  computed: {
-    options(){
-      return this.movieNames.map(movieName => {
-        return {
-          label: movieName.title,
-          id: movieName.pk
-        }
-      })
-    }
-  },
-  created(){
-    axios({
-      method: 'get',
-      url: `${SERVER_URL}/movies/name/`
-    }).then(res => {
-      console.log(res)
-      this.movieNames = res.data
-    })
-  },
   methods: {
     logout() {
       this.$store.dispatch("logout");

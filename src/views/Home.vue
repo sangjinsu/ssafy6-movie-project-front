718e52a11beb19224db573ed93ab8783dbfcc218
jinsuSang <climbplant39@kakao.com>
2021-11-22 21:13:06 +0900

    feat: seongbin css good

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="home">
-    <span>
-      <router-link :to="{ name: 'Login' }">Login</router-link>
-    </span>
-    <span></span>
-  </div>
+  <b-container style="height: 100vh">
+      <router-link :to="{ name: 'Login' }"
+       class="text-decoration-none text-white float-end pt-5">
+        <b-button  size="lg">Login</b-button>
+      </router-link>
+      <div class="h-100 d-flex align-items-center">
+      <figure class="text-start">
+        <blockquote class="blockquote text-white ">
+          <h1>취향에 따라 골라보는 다양한<br> 컨텐츠가 모두 한자리에</h1>
+        </blockquote>
+        <figcaption class="blockquote-footer text-white">
+          <h3>지금 바로 회원이 되어보세요</h3>
+        </figcaption>
+      </figure>
+      </div>
+
+
+  </b-container>
 </template>
 
 <script>
@@ -13,3 +25,4 @@ export default {
   components: {},
 };
 </script>
+

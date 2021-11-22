d791f563708eb8bc70239603e880bc0a1b25cf16
jinsuSang <climbplant39@kakao.com>
2021-11-22 21:58:04 +0900

    feat: login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,15 @@
         <b-button  size="lg">Login</b-button>
       </router-link>
       <div class="h-100 d-flex align-items-center">
-      <figure class="text-start">
-        <blockquote class="blockquote text-white ">
-          <h1>취향에 따라 골라보는 다양한<br> 컨텐츠가 모두 한자리에</h1>
-        </blockquote>
-        <figcaption class="blockquote-footer text-white">
-          <h3>지금 바로 회원이 되어보세요</h3>
-        </figcaption>
-      </figure>
+        <figure class="text-start">
+          <blockquote class="blockquote text-white ">
+            <h1>취향에 따라 골라보는 다양한<br> 컨텐츠가 모두 한자리에</h1>
+          </blockquote>
+          <figcaption class="blockquote-footer text-white">
+            <h3>지금 바로 회원이 되어보세요</h3>
+          </figcaption>
+        </figure>
       </div>
-
-
   </b-container>
 </template>
 
--- src/views/accounts/Login.vue
+++ src/views/accounts/Login.vue
@@ -1,24 +1,39 @@
 <template>
-  <div>
-    <span>
-      <router-link :to="{ name: 'Signup' }">Signup</router-link>
-    </span>
-    <h1>Login</h1>
-    <div>
-      <label for="username">name: </label>
-      <input type="text" id="username" v-model="credentials.username" />
+  <b-container style="height: 100vh">
+      <router-link :to="{ name: 'Signup' }"
+        class="text-decoration-none text-white float-end pt-5">
+        <b-button  size="lg">Signup</b-button>
+      </router-link>
+   
+    <div 
+      class="text-white
+        h-100
+        d-flex
+        align-items-center
+        justify-content-center">
+      <div style="background-color: #353b48" class="rounded p-5">
+        <h1 class="mb-4">로그인</h1>
+        <div class="mb-3 text-start">
+          <label for="username" class="form-label">아이디</label>
+          <input type="text" id="username" class="form-control" v-model="credentials.username" />
+        </div>
+        <div class="mb-3 text-start">
+          <label for="password" class="form-label">비밀번호</label>
+          <input
+            type="password"
+            @keyup.enter="login"
+            id="password"
+            v-model="credentials.password"
+            class="form-control"
+          />
+        </div>
+        <div class="d-grid gap-2">
+          <b-button @click="login" class="btn-login btn-block">LOGIN</b-button>
+        </div>
+      </div>
     </div>
-    <div>
-      <label for="password">password: </label>
-      <input
-        type="password"
-        @keyup.enter="login"
-        id="password"
-        v-model="credentials.password"
-      />
-      <button @click="login">login</button>
-    </div>
-  </div>
+
+  </b-container>
 </template>
 
 <script>
@@ -55,4 +70,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.btn-login,
+.btn-login:hover,
+.btn-login:active,
+.btn-login:visited,
+.btn-login:focus {
+    background-color: #c23616;
+    border-color: #c23616;
+}
+</style>

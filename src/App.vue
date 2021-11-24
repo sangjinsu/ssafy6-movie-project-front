6baaa21161aee9124f2a5b16fc14b15e3ca3625c
seongbiny <s30ngb1n@gmail.com>
2021-11-24 11:10:52 +0900

    feat: font

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,13 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
+body {
+  font-family: "Noto Sans KR", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: "Noto Sans KR", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
